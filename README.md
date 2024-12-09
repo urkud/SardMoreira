20b27a0d433e3ba178461da30324f6647d8e8312
Yury G. Kudryashov <urkud@urkud.name>
2024-12-09 09:40:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Moreira's version of Sard's Theorem
+
+This project aims to formalize Moreira's version of Sard's Theorem.
---
+++ SardMoreira.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `SardMoreira` library.
+-- Import modules here that should be built as part of the library.
+import SardMoreira.Basic
