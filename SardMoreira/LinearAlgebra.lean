6a97a8d1109bf19b0e9b176dd4302d77b68a8e63
Yury G. Kudryashov <urkud@urkud.name>
2025-11-29 12:32:53 -0600

    Get proofs from Aristotle, update them to match my preferences

--- SardMoreira/LinearAlgebra.lean
+++ SardMoreira/LinearAlgebra.lean
@@ -10,23 +10,36 @@ variable {E F G H : Type*}
   [AddCommGroup H] [Module ℝ H]
 
 
-theorem LinearMap.finrank_eq_finrank_iff_comp_eq_zero_of_comp_surjective
-    [FiniteDimensional ℝ E]
-    [FiniteDimensional ℝ F]
-    [FiniteDimensional ℝ G]
-    [FiniteDimensional ℝ H]
-    (f : E × F →ₗ[ℝ] G × H) (hsurj : Surjective (.fst ℝ G H ∘ₗ f))
-    (hzero : .fst ℝ G H ∘ₗ f ∘ₗ .inr ℝ E F = 0) :
-    finrank ℝ (range f) = finrank ℝ G ↔ snd ℝ G H ∘ₗ f ∘ₗ .inr ℝ E F = 0 := by
-  sorry
+namespace LinearMap
 
-theorem LinearMap.finrank_range_prod_fst_iff_comp_inr_eq_zero
-    [FiniteDimensional ℝ E]
-    [FiniteDimensional ℝ F]
-    [FiniteDimensional ℝ G]
-    (f : E × F →ₗ[ℝ] G) :
+theorem rank_prod_fst (f : E × F →ₗ[ℝ] G) :
+    rank (prod (fst ℝ E F) f) = (Module.rank ℝ E).lift + (f ∘ₗ inr ℝ E F).rank.lift := by
+  -- The range of the product map is isomorphic
+  -- to the direct sum of the ranges of the first projection and the function f.
+  have h_range : range (prod (fst ℝ E F) f) ≃ₗ[ℝ] E × range (f ∘ₗ (inr ℝ E F)) := by
+    refine LinearEquiv.ofBijective ?_ ⟨?_, ?_⟩;
+    · refine prod (fst ℝ _ _ ∘ₗ Submodule.subtype _)
+        (codRestrict _ ((snd _ _ _ - f ∘ₗ inl _ _ _ ∘ₗ fst _ _ _) ∘ₗ Submodule.subtype _) ?_)
+      rintro ⟨_, ⟨x, y⟩, rfl⟩
+      simp [← map_sub]
+    · rintro ⟨_, ⟨x, y⟩, rfl⟩ ⟨_, ⟨x', y'⟩, rfl⟩ h
+      obtain rfl : x = x' := by simpa using congr($h |>.1)
+      simp_all [← Subtype.val_inj]
+    · rintro ⟨x, _, y, rfl⟩
+      refine ⟨⟨_, (x, y), rfl⟩, ?_⟩
+      simp [← Subtype.val_inj, ← map_sub]
+  simpa using h_range.lift_rank_eq
+
+variable [FiniteDimensional ℝ E] [FiniteDimensional ℝ F]
+
+theorem finrank_range_prod_fst (f : E × F →ₗ[ℝ] G) :
+    finrank ℝ (range <| prod (fst ℝ E F) f) =
+      Module.finrank ℝ E + finrank ℝ (range <| f ∘ₗ inr ℝ E F) := by
+  rw [Module.finrank, ← LinearMap.rank, rank_prod_fst, Cardinal.toNat_add] <;>
+    simp [finrank, Module.rank_lt_aleph0]
+
+theorem finrank_range_prod_fst_iff_comp_inr_eq_zero (f : E × F →ₗ[ℝ] G) :
     finrank ℝ (range (prod (fst ℝ E F) f)) = finrank ℝ E ↔ f ∘ₗ inr ℝ E F = 0 := by
-  rw [finrank_eq_finrank_iff_comp_eq_zero_of_comp_surjective]
-  · simp [DFunLike.ext_iff]
-  · simp [Prod.fst_surjective]
-  · simp [DFunLike.ext_iff]
+  simp [finrank_range_prod_fst, range_eq_bot]
+
+end LinearMap
